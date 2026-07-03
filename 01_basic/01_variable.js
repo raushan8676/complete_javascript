ef127003b81b23d798f8a25383f1107d4beb9115
Raushan kumar <165465757+raushan8676@users.noreply.github.com>
2026-07-03 14:30:30 +0000

    variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+// Variable : store data
+
+var AccontName = "RAushan Kumar"
+let Ammount = 2000
+const AccountId = 134578
+
+AccountType = "Saving"
+
+// AccountId = 12345         const never changes
+console.log(AccountId);
+
+console.table([AccontName,Ammount,AccountType,AccountId])
+
+
+// Not Used var : because of issues in block scope and function scope
