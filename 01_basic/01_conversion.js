6520caf9d33a45e0d430ef2d253888970d95746c
Raushan kumar <165465757+raushan8676@users.noreply.github.com>
2026-07-04 17:11:52 +0000

    Conversion

---
+++ 01_basic/01_conversion.js
@@ -0,0 +1,37 @@
+// conversion 
+
+// number conversion
+
+let scoreA = "33";
+let scoreB = "33aabc";
+let scoreC = true;
+let scoreD = null;
+let ScoreE;
+
+console.table([
+    Number(scoreA),
+    Number(scoreB),
+    Number(scoreC),
+    Number(scoreD),
+    Number(ScoreE)
+ ])
+
+// boolean conversion 
+
+console.table([
+    Boolean(scoreA),
+    Boolean(scoreB),
+    Boolean(scoreC),
+    Boolean(scoreD),
+    Boolean(ScoreE)
+ ])
+
+ // String conversion
+
+ console.table([
+    String(scoreA),
+    String(scoreB),
+    String(scoreC),
+    String(scoreD),
+    String(ScoreE)
+ ])
